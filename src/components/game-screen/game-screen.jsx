e01f7fb16122a3b5a567c9a0f820c6175f2204f0
Anisa <aagermanova@gmail.com>
2020-07-03 21:50:05 -0700

    Добавит проверку ответов, подключит моки для store, подключит компонент game-screen к store, добавит компонент Mistakes.

--- src/components/game-screen/game-screen.jsx
+++ src/components/game-screen/game-screen.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {connect} from "react-redux";
 import {GameType} from "../../const.js";
+import Mistakes from "../mistakes/mistakes.jsx";
 
 
 const GameScreen = (props) => {
   const {
     type,
     children,
+    mistakes,
   } = props;
 
   return (
@@ -17,11 +20,14 @@ const GameScreen = (props) => {
           <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
         </a>
 
-        <div className="game__mistakes">
-          <div className="wrong" />
-          <div className="wrong" />
-          <div className="wrong" />
-        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+          <circle className="timer__line" cx="390" cy="390" r="370"
+            style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
+        </svg>
+
+        <Mistakes
+          count={mistakes}
+        />
       </header>
 
       {children}
@@ -36,7 +42,13 @@ GameScreen.propTypes = {
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
   ]).isRequired,
+  mistakes: PropTypes.number.isRequired,
 };
 
 
-export default GameScreen;
+const mapStateToProps = (state) => ({
+  mistakes: state.mistakes,
+});
+
+export {GameScreen};
+export default connect(mapStateToProps)(GameScreen);
