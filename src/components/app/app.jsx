d24fcccda55a84b94b95f3558fab0eae0ab03ef5
Anisa <aagermanova@gmail.com>
2020-07-08 19:14:05 -0700

    Оптимизирует прозводительность выбора жанра

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,7 +8,7 @@ import ArtistQuestionScreen from "../artist-question-screen/artist-question-scre
 import GameScreen from "../game-screen/game-screen.jsx";
 import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
 import {GameType} from "../../const.js";
-import withAudioPlayer from "../../hocs/with-audio-player/with-audio-player.js";
+import withAudioPlayer from "../../hocs/with-audio/with-audio.js";
 
 const GenreQuestionScreenWrapped = withAudioPlayer(GenreQuestionScreen);
 const ArtistQuestionScreenWrapped = withAudioPlayer(ArtistQuestionScreen);
